2f008cfd18a2770342c809a5df8d6a44a17d9c32
Renata Montanhana <112817008+renatamontanhana@users.noreply.github.com>
2023-02-06 21:50:32 -0400

    Add files via upload

--- exercicios_c.c
+++ exercicios_c.c
@@ -63,3 +63,54 @@ int main() {
   printf("%i\n", myNum);
   return 0;
 }
+
+#include <stdio.h>
+
+int main() {
+  float myFloatNum = 3.5;
+
+  printf("%f\n", myFloatNum); // Default will show 6 digits after the decimal point
+  printf("%.1f\n", myFloatNum); // Only show 1 digit
+  printf("%.2f\n", myFloatNum); // Only show 2 digits
+  printf("%.4f", myFloatNum);   // Only show 4 digits
+  return 0;
+}
+
+#include <stdio.h>
+
+int main() {
+    int score = 85;
+    if (score >= 90) {
+        printf("Grade: A\n");
+    } else if (score >= 80) {
+        printf("Grade: B\n");
+    } else if (score >= 70) {
+        printf("Grade: C\n");
+    } else if (score >= 60) {
+        printf("Grade: D\n");
+    } else {
+        printf("Grade: F\n");
+    }
+    return 0;
+}
+
+#include <stdio.h>
+
+int main() {
+  const int myNum = 15;
+  myNum = 10;
+  
+  printf("%d", myNum);
+  return 0;
+}
+
+#include <stdio.h>
+
+int main() {
+  const int minutesPerHour = 60;
+  const float PI = 3.14;
+
+  printf("%d\n", minutesPerHour);
+  printf("%f\n", PI);
+  return 0;
+}
