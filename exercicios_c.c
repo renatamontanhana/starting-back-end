c695dc6d2c2cff59b260c94288162423e71a1ca7
Renata Montanhana <112817008+renatamontanhana@users.noreply.github.com>
2023-02-09 22:05:56 -0400

    Add files via upload

--- exercicios_c.c
+++ exercicios_c.c
@@ -136,3 +136,72 @@ int main() {
   printf("%c\n", myLetter);
   return 0;
 }
+
+#include <stdio.h>
+
+int main() {
+  const int minutesPerHour = 60;
+  const float PI = 3.14;
+
+  printf("%d\n", minutesPerHour);
+  printf("%f\n", PI);
+  return 0;
+}
+
+#include <stdio.h>
+
+int main() {
+  if (20 > 18) {
+    printf("20 is greater than 18");
+  }  
+  return 0;
+}
+#include <stdio.h>
+
+int main() {
+  int x = 20;
+  int y = 18;
+  if (x > y) {
+    printf("x is greater than y");
+  }  
+  return 0;
+}
+#include <stdio.h>
+
+int main() {
+  int time = 20;
+  if (time < 18) {
+    printf("Good day.");
+  } else {
+    printf("Good evening.");
+  }
+  return 0;
+}
+#include <stdio.h>
+
+int main() {
+  int time = 22;
+  if (time < 10) {
+    printf("Good morning.");
+  } else if (time < 20) {
+    printf("Good day.");
+  } else {
+    printf("Good evening.");
+  }
+  return 0;
+}
+#include <stdio.h>
+
+int main() {
+  int myNum = 10;
+  
+  if (myNum > 0) {
+    printf("The value is a positive number.");
+  } else if (myNum < 0) {
+    printf("The value is a negative number.");
+  } else {
+    printf("The value is 0.");
+  }
+  
+  return 0;
+}
