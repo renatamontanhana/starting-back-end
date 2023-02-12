923436e31e4c7afd3f918f385ef4d00e57267691
Renata Montanhana <112817008+renatamontanhana@users.noreply.github.com>
2023-02-11 22:25:17 -0400

    Add files via upload

--- exercicios_c.c
+++ exercicios_c.c
@@ -205,3 +205,37 @@ int main() {
   
   return 0;
 }
+
+#include <stdio.h>
+
+int main() {
+  int num1, num2;
+  
+  printf("Enter two numbers: ");
+  scanf("%d%d", &num1, &num2);
+
+  if (num1 > num2) {
+    printf("%d is greater than %d\n", num1, num2);
+  } else if (num2 > num1) {
+    printf("%d is greater than %d\n", num2, num1);
+  } else {
+    printf("%d and %d are equal\n", num1, num2);
+  }
+
+  return 0;
+}
+#include <stdio.h>
+
+int main() {
+    printf("\n");
+    printf("      *       \n");
+    printf("    *****     \n");
+    printf("  *****   \n");
+    printf("  *****   \n");
+    printf("    *****     \n");
+    printf("      *       \n");
+    printf("        *         \n");
+    printf("      Renata      \n");
+    printf("\n");
+    return 0;
+}
